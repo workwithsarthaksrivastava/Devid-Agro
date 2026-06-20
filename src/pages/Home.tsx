6604201e508c4fea368debd82ee4a6b45c74138c
workwithsarthaksrivastava <workwithsarthaksrivastava@gmail.com>
2026-06-20 21:20:48 +0530

    refactor: remove Founders section from homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Hero } from '../components/Hero';
 import { About } from '../components/About';
-import { Founders } from '../components/Founders';
 import { Products } from '../components/Products';
 import { Services } from '../components/Services';
 import { WhyChooseUs } from '../components/WhyChooseUs';
@@ -14,7 +13,6 @@ export function Home() {
     <>
       <Hero />
       <About />
-      <Founders />
       <Products />
       <Services />
       <WhyChooseUs />
